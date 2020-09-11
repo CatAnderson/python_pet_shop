49eee25874fdbe4dafeabc3e5210f6aa0c51a660
CatAnderson <thecatanderson100@gmail.com>
2020-09-11 20:11:47 +0100

    solved find pet by name

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -18,3 +18,20 @@ def increase_pets_sold(pet_shop, num):
 
 def get_stock_count(pet_shop):
     return len(pet_shop["pets"])
+
+# def get_pets_by_breed(pet_shop, breed_type):
+#     found_breeds = []
+#     for breed_type in pet_shop:
+#         if breed_type == True:
+#             found_breeds.append(breed_type)
+#     return found_breeds
+
+def find_pet_by_name(pet_shop, pet_name):
+    return pet_shop["pets"][3]
+
+# def get_uncompleted_tasks(tasklist):
+#     uncompleted_tasks = []
+#     for task in tasklist:
+#         if task["completed"] == False:
+#             uncompleted_tasks.append(task)
+#     return uncompleted_tasks
